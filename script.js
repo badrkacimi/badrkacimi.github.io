cc287a3dcfbca4a68c554b87a743fd97aa5a0000
badrkacimi <badrvkacimi@gmail.com>
2024-11-04 02:29:11 +0100

    new portfolio

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Smooth scroll for internal links
+// Smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
+  });
 });
